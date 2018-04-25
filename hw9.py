07a7201f211088406adb7f20bf6b632f9d60954c
Zz2318 <35982554+Zz2318@users.noreply.github.com>
2018-04-24 20:38:19 -0400

    Update hw9.py

--- hw9.py
+++ hw9.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flast import Flask, request, render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def get(): 
-  quote = f.read().split('\n')
- 
+
+def get():
+	with open ('inspiration.txt') as fp:
+		quotes =  fp.read().split('\n')
+	from random import randrange
+	random_index = randrange(0,len(quotes))
+	quotes=quotes[random_index]
+
+	return render_template('quote.html', quotes=quotes)
